496b5d3dc0b8ce052b1fa038beb4d5089b400a44
Nicolás García <nicolasg.ngl@gmail.com>
2022-01-28 10:42:18 -0500

    cambios

---
+++ daily-bits/src/Components/inicio.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 
+const inicio = () => {
+    
+  return (
+  <div>
+    <h3>Practica tus conocimientos en la categoría que prefieras.</h3>
+    <div> 
+      <Link></Link>
+    </div>
+  </div>
+  );
+};
+
+export default inicio;
