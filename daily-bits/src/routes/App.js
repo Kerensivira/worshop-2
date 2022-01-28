a9f31d89fded911b7342b2e40ab29b177b6bd972
Alexandra Sivira <kerensivira@gmail.com>
2022-01-28 10:56:04 -0500

    gh

--- daily-bits/src/routes/App.js
+++ daily-bits/src/routes/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from './logo.svg';
 import { Home } from '../Components/Home';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <index/>
     </div>
   );
 }
