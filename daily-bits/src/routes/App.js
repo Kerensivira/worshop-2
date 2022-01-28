c743b0dc5938c4061d5e5eb800e69c407ae9a3a1
Alexandra Sivira <kerensivira@gmail.com>
2022-01-28 11:13:03 -0500

    hgt

--- daily-bits/src/routes/App.js
+++ daily-bits/src/routes/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 36acb6ed9ed6e912d3370815d3918d8a28cc89d1
 import { Home } from '../Components/Home';
 import { Logo } from '../Styles/HomeStyles';
 
@@ -10,16 +6,8 @@ import { Logo } from '../Styles/HomeStyles';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Home />
-=======
       <Home/>
-<<<<<<< HEAD
-      <index/>
->>>>>>> 36acb6ed9ed6e912d3370815d3918d8a28cc89d1
-=======
       <Logo/>
->>>>>>> d3601d7496e2ad112f456e99225b37f25dc98f36
     </div>
   );
 }
