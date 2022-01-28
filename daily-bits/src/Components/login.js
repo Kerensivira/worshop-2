9e14ef104f29d05333ef591d7c792e5135e7a5a7
Alexandra Sivira <kerensivira@gmail.com>
2022-01-28 12:33:46 -0500

    ko

--- daily-bits/src/Components/login.js
+++ daily-bits/src/Components/login.js
@@ -1,36 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Logo, Iniciar, Ingrese, Olvidar, CrearCuenta} from "../Styles/HomeStyles";
 
 export const Login = () => {
+  return(
 
+<div>
+    <Logo>
+      <h1>(..)</h1>
+      <h3>Daily Bits.</h3>
+    </Logo>
 
+    <Iniciar>
+      <h1>Iniciar Sesión</h1>
+    </Iniciar>
 
-  return 
-  (<div>
+    <Ingrese>
+      <input type="email" pattern=".User@email.com" />
+      <input type="Password" pattern={6} />
+    </Ingrese>
 
-      <Logo>
-          <h1>(..)</h1>
-          <h3>Daily Bits.</h3>
-      </Logo>
+    <Olvidar>
+      <p>¿Olvidó su contraseña?</p>
+      <a href="#" alt="Ingrese aquí." />
+    </Olvidar>
 
-      <Iniciar-Sesion>
-    <h1>Iniciar Sesión</h1>
-          </Iniciar-Sesion>
-
-          <Ingrese-correo>
-          <input type="email" pattern=".User@email.com"/>
-          <input type="Password" pattern= {6.}/>
-          </Ingrese-correo>
-
-          <Olvidar>
-           <p>¿Olvidó su contraseña?</p><a href='#'alt="Ingrese aquí."/>
-          </Olvidar>
-
-          <Crear-Cuenta>
-            <p>Aún no tienes cuenta</p><a href='#' alt="Inscribirse."></a>
-          </Crear-Cuenta>
-
-  </div>);
-
-
-
+    <CrearCuenta>
+      <p>Aún no tienes cuenta</p>
+      <a href="#" alt="Inscribirse."></a>
+    </CrearCuenta>
+  </div>
+  )
+  
 };
--- daily-bits/src/routes/App.js
+++ daily-bits/src/routes/App.js
@@ -1,11 +1,9 @@
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 import { Home } from '../Components/Home';
 import { Login } from '../Components/login';
 import { Logo } from '../Styles/HomeStyles';
 
-
-<<<<<<< HEAD
 export const App = () => {
       return (
             <div className="App">
@@ -18,16 +16,5 @@ export const App = () => {
                   </BrowserRouter>
             </div>
       );
-=======
-function App() {
-  return (
-    <div className="App">
-      <Login />
-      <Home />
-      <Logo/>
-    </div>
-  );
->>>>>>> 27351bd1962eb2c41ecd76eb6490a7e00450d6ec
+
 }
-
-
