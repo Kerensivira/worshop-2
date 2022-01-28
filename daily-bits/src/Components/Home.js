f3f0507736f975ada01f5ff1020ffa0b2975b3a4
Alexandra Sivira <kerensivira@gmail.com>
2022-01-28 10:02:18 -0500

    f

---
+++ daily-bits/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const HomeStyles = () => {
+  return 
+  (<div>
+      <Logo>
+          <h1>(..)</h1>
+          <h3>Daily Bits.</h3>
+      </Logo>
+
+  </div>);
+
+
+
+};
