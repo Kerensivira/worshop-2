1b3cccf9bfd36aff1113c2066c5504526b231d6b
Alexandra Sivira <kerensivira@gmail.com>
2022-01-28 11:43:02 -0500

    n

--- daily-bits/src/routes/App.js
+++ daily-bits/src/routes/App.js
@@ -7,7 +7,6 @@ import { Logo } from '../Styles/HomeStyles';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Login />
       <Home />
       <Logo/>
@@ -15,7 +14,6 @@ function App() {
 =======
       <Home/>
       <Logo/>
->>>>>>> 8e4da40a2e4fd41079ef994ddd1c060588c77f7f
     </div>
   );
 }
