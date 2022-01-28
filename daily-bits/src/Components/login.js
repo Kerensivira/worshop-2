fbe0c4f210d2b12d98af7c86eeef7e0f4f4ac730
Alexandra Sivira <kerensivira@gmail.com>
2022-01-28 10:20:59 -0500

    h

---
+++ daily-bits/src/Components/login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export const HomeStyles = () => {
+  return 
+  (<div>
+
+      <Logo>
+          <h1>(..)</h1>
+          <h3>Daily Bits.</h3>
+      </Logo>
+
+      <Iniciar-Sesion>
+<h1>Iniciar Sesión</h1>
+          </Iniciar-Sesion>
+
+          <Boton-Google>
+          <Link to="#"><Button type='button'>Continuar con Google.</Button></Link> <br></br>
+          </Boton-Google>
+
+          <Crear-Cuenta>
+          <label for="email" placeholder='Ingresa tu e-mail'></label>
+
+       <input type="email" pattern=".User@email.com" size="30" required>
+
+          </Crear-Cuenta>
+
+  </div>);
+
+
+
+};
